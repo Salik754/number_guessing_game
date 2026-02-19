ad3505fecc778c714395d20fd541f3398c06fb07
Salik754 <salikahmadn505@deltalearns.ca>
2026-02-18 22:40:23 -0800

    updated the docstring, added the variables in the function, because they weren't working before and I was getting an error
    yh, overall, I guess I'm all done.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """
-Making a number guessing game with rounds, 
+Making a number guessing game with rounds, where the user can select a number range and the number of attempts they want.
+The game will give feedback on whether the guess is too high, too low, or correct.
+After each round, the user will be asked if they want to play again. 
+The game will continue until the user decides to stop playing.
 """
 #imported a random number to generate a random number
 import random
@@ -27,30 +30,40 @@ def play_game():
     low = get_valid_int("Enter the lowest number of the range: ")
     high = get_valid_int("Enter the highest number of the range: ")
 
+    #make sure that that the range is valid
+    while high <= low:
+        print("Invalid range. The highest number must be greater than the lowest number.")
+        high = get_valid_int("Enter the highest number of the range: ")
 
-#gonna ask for attempts
-max_attempts = get_valid_int("How many attempts do you want? ")
+    #gonna ask for attempts
+    max_attempts = get_valid_int("How many attempts do you want? ")
 
-#generate a random number
-secret_number = random.randint(low, high)
-#the number of attempts used
-attempts_used = 0
+    #generate a random number
+    secret_number = random.randint(low, high)
+    #the number of attempts used
+    attempts_used = 0
 
 
-#the game loop
-while attempts_used < max_attempts:
-    guess = get_valid_int("Enter your guess: ")
-    attempts_used += 1
+    #the game loop
+    while attempts_used < max_attempts:
+        guess = get_valid_int("Enter your guess: ")
+        attempts_used += 1
 
-    if guess < secret_number:
-        print("Too low!")
-    elif guess > secret_number:
-        print("Too high!")
-    else:
-        print(f"Congratulations! You guessed the number in {attempts_used} attempts.")
+        if guess < secret_number:
+            print("Too low!")
+        elif guess > secret_number:
+            print("Too high!")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts_used} attempts.")
+            return
+
+     #if user runs out of attempts
+    print(f"Sorry, you've run out of attempts. The number was {secret_number}.")
+
+
+
     
-#if user runs out of attempts
-print(f"Sorry, you've run out of attempts. The number was {secret_number}.")
+
 
 #gonna ask for their name
 name=input("What's your name? ").strip()
